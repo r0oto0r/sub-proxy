da33430a7c1dffcf2662473720b17beb4e9e2e5d
ben <ben@forkbomb.dev>
2025-05-25 17:28:19 +0200

    Fixed stuff.

--- container_start.sh
+++ container_start.sh
@@ -5,7 +5,7 @@ set -e
 echo "Starting sub-proxy container..."
 
 echo "Starting WhisperLiveKit server in background..."
-python3 -m whisperlivekit.server --model large-v3 --host 0.0.0.0 --port 8000 &
+pipx run --spec git+https://github.com/QuentinFuxa/WhisperLiveKit.git whisperlivekit.server --model large-v3 --host 0.0.0.0 --port 8000 &
 
 # Wait a moment for WhisperLiveKit to start
 sleep 5
