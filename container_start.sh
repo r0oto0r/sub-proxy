13d35442ffa285236d4dd83d1dfd42fd3b359b3e
ben <ben@forkbomb.dev>
2025-05-26 22:31:53 +0200

    Fixed stuff.

--- container_start.sh
+++ container_start.sh
@@ -8,7 +8,7 @@ huggingface-cli login --token $HUGGINGFACE_TOKEN
 
 echo "Starting WhisperLiveKit server in background..."
 whisperlivekit-server \
-	--model large-v3-turbo \
+	--model small \
 	--host 0.0.0.0 \
 	--port 8000 \
 	--language de \
