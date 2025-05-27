695a7c2e8b09344351daf2173450cd757e37d751
ben <ben@forkbomb.dev>
2025-05-27 11:12:17 +0200

    Fixed stuff.

--- container_start.sh
+++ container_start.sh
@@ -14,7 +14,6 @@ whisperlivekit-server \
 	--language de \
 	--task translate \
 	--diarization \
-	--chunk_length_s 1 \
 	--buffer_trimming segment 2>&1 | \
 	while IFS= read -r line; do
 		echo -e "\033[36m[WhisperLiveKit]\033[0m $line"
